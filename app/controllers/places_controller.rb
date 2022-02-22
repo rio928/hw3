c3c4198d308ed6415c5ded8d0fe37bc23592e7c2
rio928 <rionex928@yahoo.co.jp>
2022-02-22 22:12:49 +0000

    changed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,15 @@
 class PlacesController < ApplicationController
     def index
-        @places = Place.ApplicationController
+        @places = Place.all
     end
 
     def new
         @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
     end
 end
